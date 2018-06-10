cb024dfbe0c10b019602392218e795eb441ac2f9
qianying1 <1101627399@qq.com>
2018-06-10 23:29:33 +0800

    加入了spring secure

---
+++ src/main/java/com/qhb/service/UserDetailService.java
@@ -0,0 +1,11 @@
+package com.qhb.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by qianying on 2018/6/10.
+ */
+public interface UserDetailService extends UserDetailsService {
+
+    void test();
+}
---
+++ src/main/java/com/qhb/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,40 @@
+package com.qhb.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.qhb.service.UserDetailService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by qianying on 2018/6/10.
+ */
+@Service(version = "1.0.0")
+public class UserDetailServiceImpl implements UserDetailService {
+
+    public void test(){
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        //SUser对应数据库中的用户表，是最终存储用户和密码的表，可自定义
+        //本例使用SUser中的email作为用户名:
+        SUser user = suserService.findUserByEmail(userName);
+
+        if (user == null) {
+
+            throw new UsernameNotFoundException("UserName " + userName + " not found");
+
+        }
+
+        // SecurityUser实现UserDetails并将SUser的Email映射为username
+        SecurityUser securityUser = new SecurityUser(user);
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        return securityUser;
+    }
+}
